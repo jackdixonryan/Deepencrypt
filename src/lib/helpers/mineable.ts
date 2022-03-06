9ba1647ce2cd74b73210be6f6448cf2b9af2ce7b
jackdixonryan <jack.dixon.ryan@gmail.com>
2022-03-06 09:51:29 -0700

    Mining resource types fully mapped.

--- src/lib/helpers/mineable.ts
+++ src/lib/helpers/mineable.ts
@@ -37,7 +37,7 @@ export const types: MineableType[] = [
   },
   {
     name: "encryptedReferences",
-    requiredLevel: 11,
+    requiredLevel: 10,
     xp: 4.5,
     timeToComplete: 1, 
     yields: [
@@ -52,11 +52,192 @@ export const types: MineableType[] = [
   }, 
   {
     name: "corruptedMetadata",
-    requiredLevel: 11,
+    requiredLevel: 20,
     xp: 4.5,
     timeToComplete: 1,
-    yields: [],
+    yields: [
+      {
+        name: "corrupted-data-packet",
+        quantity: 1, 
+        probabilityToLoot: 0.1
+      }
+    ],
     skill: "mining",
     description: "at least only the metadata got corrupted"
+  },
+  {
+    name: "intactBlock",
+    requiredLevel: 30,
+    xp: 1,
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "preserved-data-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1,
+      }
+    ],
+    skill: "mining",
+    description: "but where is the old chain?"
+  }, 
+  {
+    name: "securedBlock",
+    requiredLevel: 40,
+    xp: 1,
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "well-secured-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1
+      }
+    ],
+    skill: "mining"
+  },
+  {
+    name: "pristineMetadata",
+    requiredLevel: 40,
+    xp: 1,
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "pristine-data-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1,
+      }
+    ],
+    skill: "mining"
+  },
+  {
+    name: "unbreakableBlock",
+    requiredLevel: 50,
+    xp: 1, 
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "immutable-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1,
+      }
+    ], 
+    skill: "mining"
+  },
+  {
+    name: "executiveBlock",
+    requiredLevel: 60,
+    xp: 1,
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "vip-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1
+      },
+      {
+        name: "uncommon-encryption",
+        quantity: 1,
+        probabilityToLoot: 0.01,
+      }
+    ],
+    skill: "mining"
+  },
+  {
+    name: "erwdMetadata", 
+    requiredLevel: 60,
+    xp: 1,
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "erwd-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1
+      }
+    ],
+    skill: "mining"
+  },
+  {
+    name: "classifiedBlock",
+    requiredLevel: 70,
+    xp: 1,
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "classified-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1
+      },
+      {
+        name: "rare-encryption",
+        quantity: 1,
+        probabilityToLoot: 0.01,
+      }
+    ],
+    skill: "mining"
+  },
+  {
+    name: "mightyHash",
+    requiredLevel: 70,
+    xp: 1,
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "might-hash-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1
+      }
+    ],
+    skill: "mining"
+  },
+  {
+    name: "foundationalBlock",
+    requiredLevel: 80,
+    xp: 1,
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "foundational-data-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1
+      },
+      {
+        name: "epic-encryption",
+        quantity: 1,
+        probabilityToLoot: 0.01
+      }
+    ],
+    skill: "mining"
+  },
+  {
+    name: "legendaryBlock",
+    requiredLevel: 90,
+    xp: 1,
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "legendary-data-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1
+      }, 
+      {
+        name: "legendary-encryption",
+        quantity: 1,
+        probabilityToLoot: 0.01,
+      },
+    ],
+    skill: "mining"
+  },
+  {
+    name: "legendaryMetadata",
+    requiredLevel: 90,
+    xp: 1,
+    timeToComplete: 1,
+    yields: [
+      {
+        name: "legendary-metadata-packet",
+        quantity: 1,
+        probabilityToLoot: 0.1,
+      }
+    ],
+    skill: "mining"
   }
 ];
